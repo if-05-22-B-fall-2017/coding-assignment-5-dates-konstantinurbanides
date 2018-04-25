abd1eefcb3e7cf720658f1eb8018297a5570398d
konstantinurbanides <konstantinurbanides@gmx.at>
2018-04-25 07:43:49 +0200

    Konstantin Urbanides Abgabe

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^(0?[1-9]|[12]\\d|3[01])[- /.](0?[1-9]|1[012])[- /.](19|20)?\\d\\d$");
+        Matcher m = p.matcher(dateString);
+        return m;
     }
 }
